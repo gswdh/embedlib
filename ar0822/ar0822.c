bac20a9b0650a7f2ed920debe16a6e63a7fd2fd5
George Waller <georgewaller@gswdh.co.uk>
2024-06-26 19:47:51 +0200

    Few functions begining for the AR0822 driver

---
+++ ar0822/ar0822.c
@@ -0,0 +1,77 @@
+#include "ar0822.h"
+
+#include <assert.h>
+
+void __attribute__((weak)) ar_set_nrst(const bool en)
+{
+    assert(false && "AR driver interface has not been implemented.");
+}
+
+void __attribute__((weak)) ar_set_xshutdown(const bool en)
+{
+    assert(false && "AR driver interface has not been implemented.");
+}
+
+uint8_t __attribute__((weak)) ar_get_gpio(void)
+{
+    assert(false && "AR driver interface has not been implemented.");
+    return 0;
+}
+
+ar_error_t __attribute__((weak)) ar_i2c_write(const uint16_t reg, const uint8_t *data, const uint32_t len)
+{
+    assert(false && "AR driver interface has not been implemented.");
+    return AR_I2C_FAIL;
+}
+
+ar_error_t __attribute__((weak)) ar_i2c_read(const uint16_t reg, const uint8_t *data, const uint32_t len)
+{
+    assert(false && "AR driver interface has not been implemented.");
+    return AR_I2C_FAIL;
+}
+
+static ar_error_t _ar_reg_init(const ar_reg_write_t *config, uint32_t len)
+{
+    // Go through all the register settings
+    for (uint32_t i = 0; i < len; i++)
+    {
+        ar_error_t error = AR_OK;
+        uint16_t data = 0;
+
+        // Get the value in the current state
+        error = ar_i2c_read(config[i].addr, (uint8_t *)&data, sizeof(data));
+        if (error != AR_OK)
+        {
+            return error;
+        }
+
+        // Clear the relevant bits
+        data &= ~config[i].mask;
+
+        // Set the new value
+        data |= config[i].data & config[i].mask;
+
+        // Write back to the device
+        error = ar_i2c_write(config[i].addr, (uint8_t *)&data, sizeof(data));
+        if (error != AR_OK)
+        {
+            return error;
+        }
+    }
+
+    // Went well
+    return AR_OK;
+}
+
+ar_error_t ar_init(const ar_reg_write_t *config, uint32_t len)
+{
+    // Set the register values
+    ar_error_t error = _ar_reg_init(config, len);
+
+    if (error != AR_OK)
+    {
+        return error;
+    }
+
+    return AR_INIT_FAIL;
+}
---
+++ ar0822/ar0822.h
@@ -0,0 +1,44 @@
+#ifndef _AR0822_H_
+#define _AR0822_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef enum
+{
+    AR_RESET = 0,
+    AR_RUN
+} ar_reset_t;
+
+typedef enum
+{
+    AR_OK = 0,
+    AR_INIT_FAIL,
+    AR_INVALID_CONFIG,
+    AR_I2C_FAIL,
+} ar_error_t;
+
+typedef struct
+{
+    uint16_t addr;
+    uint16_t mask;
+    uint16_t data;
+} ar_reg_write_t;
+
+#define AR_I2C_DEV_ADDR (0x55)
+
+// Interface functions
+void ar_set_nrst(const bool en);
+void ar_set_xshutdown(const bool en);
+uint8_t ar_get_gpio(void);
+ar_error_t ar_i2c_write(const uint16_t reg, const uint8_t *data, const uint32_t len);
+ar_error_t ar_i2c_read(const uint16_t reg, const uint8_t *data, const uint32_t len);
+
+// Driver functions
+ar_error_t ar_init(const ar_reg_write_t *config, uint32_t len);
+ar_error_t ar_trigger_frame(void);
+ar_error_t ar_set_gain(const float gain);
+ar_error_t ar_set_shutter_time_s(const float time_s);
+ar_error_t ar_set_resolution(const uint32_t x, const uint32_t y);
+
+#endif
