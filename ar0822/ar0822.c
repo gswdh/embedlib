996dd7cd8d5b757d4155093633876790afc12b7d
George Waller <georgewaller@gswdh.co.uk>
2024-10-25 21:35:59 +0200

    Working AR0822 driver

--- ar0822/ar0822.c
+++ ar0822/ar0822.c
@@ -20,26 +20,67 @@ void __attribute__((weak)) ar_enable_clock(const bool en)
 uint8_t __attribute__((weak)) ar_get_gpio(void)
 {
     assert(false && "AR driver interface has not been implemented.");
-    return 0;
+    return 0U;
 }
 
 ar_error_t __attribute__((weak)) ar_i2c_write(const uint16_t reg, const uint8_t *data, const uint32_t len)
 {
     assert(false && "AR driver interface has not been implemented.");
-    return AR_I2C_FAIL;
+    return AR_ERROR_I2C_FAIL;
 }
 
 ar_error_t __attribute__((weak)) ar_i2c_read(const uint16_t reg, const uint8_t *data, const uint32_t len)
 {
     assert(false && "AR driver interface has not been implemented.");
-    return AR_I2C_FAIL;
+    return AR_ERROR_I2C_FAIL;
 }
+
 void __attribute__((weak)) ar_delay_ms(const uint32_t time_ms)
 {
     assert(false && "AR driver interface has not been implemented.");
 }
 
-static ar_error_t _ar_reg_init(const ar_reg_write_t *config, uint32_t len)
+uint32_t __attribute__((weak)) ar_tick_ms(void)
+{
+    assert(false && "AR driver interface has not been implemented.");
+    return 0U;
+}
+
+static uint16_t reverse_endian(uint16_t value)
+{
+    return (value << 8) | (value >> 8);
+}
+
+static ar_error_t ar_read_reg(const uint16_t reg, uint16_t *const value)
+{
+    ar_error_t error = ar_i2c_read(reg, (uint8_t *)value, sizeof(value));
+    if (error != AR_OK)
+    {
+        return error;
+    }
+
+    // Reverse the edianess
+    *value = reverse_endian(*value);
+
+    return AR_OK;
+}
+
+static ar_error_t ar_write_reg(const uint16_t reg, uint16_t value)
+{
+    // Reverse the edianess
+    value = reverse_endian(value);
+
+    // Write back to the device
+    ar_error_t error = ar_i2c_write(reg, (uint8_t *)&value, sizeof(value));
+    if (error != AR_OK)
+    {
+        return error;
+    }
+
+    return AR_OK;
+}
+
+static ar_error_t ar_reg_init(const ar_reg_write_t *config, uint32_t len)
 {
     // Go through all the register settings
     for (uint32_t i = 0; i < len; i++)
@@ -48,7 +89,7 @@ static ar_error_t _ar_reg_init(const ar_reg_write_t *config, uint32_t len)
         uint16_t data = 0;
 
         // Get the value in the current state
-        error = ar_i2c_read(config[i].addr, (uint8_t *)&data, sizeof(data));
+        error = ar_read_reg(config[i].addr, &data);
         if (error != AR_OK)
         {
             return error;
@@ -61,11 +102,32 @@ static ar_error_t _ar_reg_init(const ar_reg_write_t *config, uint32_t len)
         data |= config[i].data & config[i].mask;
 
         // Write back to the device
-        error = ar_i2c_write(config[i].addr, (uint8_t *)&data, sizeof(data));
+        error = ar_write_reg(config[i].addr, data);
         if (error != AR_OK)
         {
             return error;
         }
+    }
+
+    // Wait for the status to be in the stream mode
+    uint16_t status = 0;
+    uint32_t t_start = ar_tick_ms();
+    while ((status & AR_REG_FRAME_STATUS_STREAM_BIT) != AR_REG_FRAME_STATUS_STREAM_BIT)
+    {
+        ar_error_t error = ar_frame_status(&status);
+        if (error != AR_OK)
+        {
+            return error;
+        }
+
+        // Timeout condition
+        if (ar_tick_ms() > (t_start + AR_INIT_SYNC_TO_MS))
+        {
+            return AR_ERROR_SYNC_TO;
+        }
+
+        // A little delay to save the CPU
+        ar_delay_ms(10);
     }
 
     // Went well
@@ -101,12 +163,54 @@ ar_error_t ar_init(const ar_reg_write_t *config, uint32_t len)
     ar_delay_ms(1000);
 
     //  Set the register values
-    ar_error_t error = _ar_reg_init(config, len);
+    ar_error_t error = ar_reg_init(config, len);
 
     if (error != AR_OK)
     {
         return error;
     }
 
+    return AR_OK;
+}
+
+ar_error_t ar_frame_status(uint16_t *const status)
+{
+    ar_error_t error = ar_read_reg(AR_REG_FRAME_STATUS, status);
+    if (error != AR_OK)
+    {
+        return error;
+    }
+
+    return AR_OK;
+}
+
+ar_error_t ar_gpio_config(uint16_t *const config)
+{
+    ar_error_t error = ar_read_reg(AR_REG_FRAME_STATUS, config);
+    if (error != AR_OK)
+    {
+        return error;
+    }
+
+    return AR_OK;
+}
+
+ar_error_t ar_trigger_frame(void)
+{
+    return AR_OK;
+}
+
+ar_error_t ar_set_gain(const float gain)
+{
+    return AR_OK;
+}
+
+ar_error_t ar_set_shutter_time_s(const float time_s)
+{
+    return AR_OK;
+}
+
+ar_error_t ar_set_resolution(const uint32_t x, const uint32_t y)
+{
     return AR_OK;
 }
--- ar0822/ar0822.h
+++ ar0822/ar0822.h
@@ -13,9 +13,10 @@ typedef enum
 typedef enum
 {
     AR_OK = 0,
-    AR_INIT_FAIL,
-    AR_INVALID_CONFIG,
-    AR_I2C_FAIL,
+    AR_ERROR_INIT_FAIL,
+    AR_ERROR_INVALID_CONFIG,
+    AR_ERROR_I2C_FAIL,
+    AR_ERROR_SYNC_TO,
 } ar_error_t;
 
 typedef struct
@@ -27,6 +28,13 @@ typedef struct
 
 #define AR_I2C_DEV_ADDR (0x10)
 
+#define AR_INIT_SYNC_TO_MS (100U)
+
+#define AR_REG_FRAME_STATUS (0x2008)
+#define AR_REG_GPIO_SELECT (0x340E)
+
+#define AR_REG_FRAME_STATUS_STREAM_BIT (0x0008)
+
 // Interface functions
 void ar_set_nrst(const bool en);
 void ar_set_xshutdown(const bool en);
@@ -35,9 +43,12 @@ uint8_t ar_get_gpio(void);
 ar_error_t ar_i2c_write(const uint16_t reg, const uint8_t *data, const uint32_t len);
 ar_error_t ar_i2c_read(const uint16_t reg, const uint8_t *data, const uint32_t len);
 void ar_delay_ms(const uint32_t time_ms);
+uint32_t ar_tick_ms(void);
 
 // Driver functions
 ar_error_t ar_init(const ar_reg_write_t *config, uint32_t len);
+ar_error_t ar_frame_status(uint16_t *const status);
+ar_error_t ar_gpio_config(uint16_t *const config);
 ar_error_t ar_trigger_frame(void);
 ar_error_t ar_set_gain(const float gain);
 ar_error_t ar_set_shutter_time_s(const float time_s);
