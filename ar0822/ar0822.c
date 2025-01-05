ebc9d7d18e236cd66a1cdba0916a9c4ef4450a43
George <georgewaller@gswdh.co.uk>
2025-01-05 16:10:45 +0000

    feat(AR0822): Gain and integration period control functions

--- ar0822/ar0822.c
+++ ar0822/ar0822.c
@@ -1,6 +1,7 @@
 #include "ar0822.h"
 
 #include <assert.h>
+#include <math.h>
 
 void __attribute__((weak)) ar_set_nrst(const bool en)
 {
@@ -80,29 +81,38 @@ static ar_error_t ar_write_reg(const uint16_t reg, uint16_t value)
     return AR_OK;
 }
 
+static ar_error_t ar_i2c_modify_reg(const uint16_t addr, const uint16_t data, const uint16_t mask)
+{
+    // Get the value in the current state
+    uint16_t current = 0;
+    ar_error_t error = ar_read_reg(addr, &current);
+    if (error != AR_OK)
+    {
+        return error;
+    }
+
+    // Clear the relevant bits
+    current &= ~mask;
+
+    // Set the new value
+    current |= data & mask;
+
+    // Write back to the device
+    error = ar_write_reg(addr, current);
+    if (error != AR_OK)
+    {
+        return error;
+    }
+
+    return AR_OK;
+}
+
 static ar_error_t ar_reg_init(const ar_reg_write_t *config, uint32_t len)
 {
     // Go through all the register settings
     for (uint32_t i = 0; i < len; i++)
     {
-        ar_error_t error = AR_OK;
-        uint16_t data = 0;
-
-        // Get the value in the current state
-        error = ar_read_reg(config[i].addr, &data);
-        if (error != AR_OK)
-        {
-            return error;
-        }
-
-        // Clear the relevant bits
-        data &= ~config[i].mask;
-
-        // Set the new value
-        data |= config[i].data & config[i].mask;
-
-        // Write back to the device
-        error = ar_write_reg(config[i].addr, data);
+        ar_error_t error = ar_i2c_modify_reg(config[i].addr, config[i].data, config[i].mask);
         if (error != AR_OK)
         {
             return error;
@@ -132,6 +142,20 @@ static ar_error_t ar_reg_init(const ar_reg_write_t *config, uint32_t len)
 
     // Went well
     return AR_OK;
+}
+
+static uint16_t ar_gain_float_to_register(const float value)
+{
+    // Scale the float value to match the fixed-point format (xxxx.yyyyyyy)
+    uint16_t fixed_point = (uint16_t)roundf(value * 128.0f);
+
+    // Ensure it fits within 11 bits
+    if (fixed_point > 0x7FF)
+    {
+        fixed_point = 0x7FF; // Clamp to max value
+    }
+
+    return fixed_point;
 }
 
 ar_error_t ar_init(const ar_reg_write_t *config, uint32_t len)
@@ -164,11 +188,13 @@ ar_error_t ar_init(const ar_reg_write_t *config, uint32_t len)
 
     //  Set the register values
     ar_error_t error = ar_reg_init(config, len);
-
     if (error != AR_OK)
     {
         return error;
     }
+
+    // Enable colour gains
+    ar_i2c_modify_reg(AR_REG_HDR_CONTROL, 0x0010, 0x0010);
 
     return AR_OK;
 }
@@ -200,13 +226,94 @@ ar_error_t ar_trigger_frame(void)
     return AR_OK;
 }
 
-ar_error_t ar_set_gain(const float gain)
+ar_error_t ar_get_gain(float *gain_db)
 {
+    uint16_t reg_value = 0;
+    if (ar_read_reg(AR_REG_GLOBAL_GAIN, &reg_value) != AR_OK)
+    {
+        return AR_ERROR_I2C_FAIL;
+    }
+
+    *gain_db = ((float)(reg_value & 0xFF)) * AR_REG_GLOBAL_GAIN_STEP;
+
+    return AR_OK;
+}
+
+ar_error_t ar_set_gain(const float gain_db)
+{
+    const uint16_t reg_value = (uint16_t)(gain_db / AR_REG_GLOBAL_GAIN_STEP) & 0x00FF;
+
+    if (reg_value > AR_REG_GLOBAL_GAIN_MAX)
+    {
+        return AR_ERROR_INVALID_PARAM;
+    }
+
+    if (ar_write_reg(AR_REG_GLOBAL_GAIN, reg_value) != AR_OK)
+    {
+        return AR_ERROR_I2C_FAIL;
+    }
+
+    return AR_OK;
+}
+
+ar_error_t ar_set_colour_gain(const float gain, const ar_colour_t colour)
+{
+    const uint16_t reg_value = ar_gain_float_to_register(gain);
+
+    switch (colour)
+    {
+    case AR_COLOUR_R:
+        if (ar_write_reg(AR_REG_GAIN_R, reg_value) != AR_OK)
+        {
+            return AR_ERROR_I2C_FAIL;
+        }
+        break;
+    case AR_COLOUR_G:
+        if (ar_write_reg(AR_REG_GAIN_G1, reg_value) != AR_OK)
+        {
+            return AR_ERROR_I2C_FAIL;
+        }
+        if (ar_write_reg(AR_REG_GAIN_G2, reg_value) != AR_OK)
+        {
+            return AR_ERROR_I2C_FAIL;
+        }
+        break;
+    case AR_COLOUR_B:
+        if (ar_write_reg(AR_REG_GAIN_B, reg_value) != AR_OK)
+        {
+            return AR_ERROR_I2C_FAIL;
+        }
+        break;
+    default:
+        return AR_ERROR_INVALID_PARAM;
+        break;
+    }
+
+    return AR_OK;
+}
+
+ar_error_t ar_get_shutter_time_s(float *time_s)
+{
+    uint16_t reg_value = 0;
+    if (ar_read_reg(AR_REG_COARSE_INT, &reg_value) != AR_OK)
+    {
+        return AR_ERROR_I2C_FAIL;
+    }
+
+    *time_s = ((float)(reg_value)) * AR_ROW_TIME_S;
+
     return AR_OK;
 }
 
 ar_error_t ar_set_shutter_time_s(const float time_s)
 {
+    const uint16_t reg_value = (uint16_t)(time_s / AR_ROW_TIME_S);
+
+    if (ar_write_reg(AR_REG_COARSE_INT, reg_value) != AR_OK)
+    {
+        return AR_ERROR_I2C_FAIL;
+    }
+
     return AR_OK;
 }
 
--- ar0822/ar0822.h
+++ ar0822/ar0822.h
@@ -17,6 +17,7 @@ typedef enum
     AR_ERROR_INVALID_CONFIG,
     AR_ERROR_I2C_FAIL,
     AR_ERROR_SYNC_TO,
+    AR_ERROR_INVALID_PARAM,
 } ar_error_t;
 
 typedef struct
@@ -26,14 +27,36 @@ typedef struct
     uint16_t mask;
 } ar_reg_write_t;
 
+typedef enum
+{
+    AR_COLOUR_RGB = 0,
+    AR_COLOUR_R,
+    AR_COLOUR_G,
+    AR_COLOUR_B,
+} ar_colour_t;
+
 #define AR_I2C_DEV_ADDR (0x10)
 
 #define AR_INIT_SYNC_TO_MS (100U)
+
+#define AR_ROW_TIME_S (0.000014524)
+
+#define AR_REG_GLOBAL_GAIN_MAX (0x77)
+#define AR_REG_GLOBAL_GAIN_STEP (0.375)
 
 #define AR_REG_FRAME_STATUS (0x2008)
 #define AR_REG_GPIO_SELECT (0x340E)
 
 #define AR_REG_FRAME_STATUS_STREAM_BIT (0x0008)
+
+#define AR_REG_GLOBAL_GAIN (0x5900)
+#define AR_REG_COARSE_INT (0x3012)
+#define AR_REG_HDR_CONTROL (0x3110)
+
+#define AR_REG_GAIN_G1 (0x3056)
+#define AR_REG_GAIN_B (0x3058)
+#define AR_REG_GAIN_R (0x305A)
+#define AR_REG_GAIN_G2 (0x305C)
 
 // Interface functions
 void ar_set_nrst(const bool en);
@@ -50,7 +73,10 @@ ar_error_t ar_init(const ar_reg_write_t *config, uint32_t len);
 ar_error_t ar_frame_status(uint16_t *const status);
 ar_error_t ar_gpio_config(uint16_t *const config);
 ar_error_t ar_trigger_frame(void);
+ar_error_t ar_get_gain(float *gain_db);
 ar_error_t ar_set_gain(const float gain);
+ar_error_t ar_set_colour_gain(const float gain, const ar_colour_t c);
+ar_error_t ar_get_shutter_time_s(float *time_s);
 ar_error_t ar_set_shutter_time_s(const float time_s);
 ar_error_t ar_set_resolution(const uint32_t x, const uint32_t y);
 
